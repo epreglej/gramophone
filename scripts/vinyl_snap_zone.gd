aa972b671b54d6368fdb1819e0aabbc0022f06f4
epreglej <eugen.preglej@gmail.com>
2026-01-02 23:42:59 +0100

    Fixed rotate function bug on vinyl snap zones.

--- scripts/vinyl_snap_zone.gd
+++ scripts/vinyl_snap_zone.gd
@@ -19,8 +19,26 @@ func pick_up_object(target: Node3D) -> void:
 	
 	super.pick_up_object(target)
 	
-	if vinyl.side == Vinyl.VinylSide.B:
-		vinyl.rotate(Vector3(1,0,0), PI)
+	_apply_vinyl_rotation()
 	
 	if label:
 		label.text = vinyl.song.title
+
+
+func _apply_vinyl_rotation() -> void:
+	if not is_instance_valid(vinyl):
+		return
+
+	var t := vinyl.global_transform
+
+	# Snap zone orientation is the reference
+	var base_basis := global_transform.basis
+
+	if vinyl.side == Vinyl.VinylSide.B:
+		# Flip 180° around local X (record flipped)
+		t.basis = base_basis * Basis(Vector3.RIGHT, PI)
+	else:
+		# Side A: match snap zone exactly
+		t.basis = base_basis
+
+	vinyl.global_transform = t
